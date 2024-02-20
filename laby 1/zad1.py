d7754e4aceb169e68f0e6be16029202850829c4d
Michał Pomirski <mpomirski@inf.ug.edu.pl>
2024-02-20 14:45:11 +0100

    podpunkt b

--- laby 1/zad1.py
+++ laby 1/zad1.py
@@ -1,5 +1,6 @@
-from datetime import date
+from datetime import date, timedelta
 import math
+from typing import Callable
 def calc_days_old(birthdate: date) -> int:
     return (date.today() - birthdate).days
 
@@ -12,7 +13,14 @@ def emotional_biorythm(birthdate: date) -> float:
 def intellectual_biorythm(birthdate: date) -> float:
     return math.sin(2*math.pi*calc_days_old(birthdate)/33)
 
-    
+def check_biorythm(birthdate: date, biorythm: Callable) -> str:
+    if biorythm(birthdate) > 0.5:
+        return "Świetnie!"
+    else:
+        if biorythm(birthdate - timedelta(days=1)) > 0.5:
+            return "Jutro bedzie lepiej"
+        else:
+            return "Nie za dobrze"
 
 def main():
     name: str = input("Podaj imię: ")
@@ -21,11 +29,14 @@ def main():
     birthday = int(input("Podaj dzień urodzenia: "))
     print(f"Hello {name}.")
     print(f"Jesteś już {calc_days_old(date(birthyear, birthmonth, birthday))} dni na świecie.")
-    print(f"Twój fizyczny biorytm wynosi {physical_biorythm(date(birthyear, birthmonth, birthday))}.")
-    print(f"Twój emocjonalny biorytm wynosi {emotional_biorythm(date(birthyear, birthmonth, birthday))}.")
-    print(f"Twój intelektualny biorytm wynosi {intellectual_biorythm(date(birthyear, birthmonth, birthday))}.")
-    
+    print(f"Twój fizyczny biorytm wynosi {physical_biorythm(date(birthyear, birthmonth, birthday)):.2f}.")
+    print(check_biorythm(date(birthyear, birthmonth, birthday), physical_biorythm))
+    print(f"Twój emocjonalny biorytm wynosi {emotional_biorythm(date(birthyear, birthmonth, birthday)):.2f}.")
+    print(check_biorythm(date(birthyear, birthmonth, birthday), emotional_biorythm))
+    print(f"Twój intelektualny biorytm wynosi {intellectual_biorythm(date(birthyear, birthmonth, birthday)):.2f}.")
+    print(check_biorythm(date(birthyear, birthmonth, birthday), intellectual_biorythm))
 
+    
 
 
 if __name__ == "__main__":
